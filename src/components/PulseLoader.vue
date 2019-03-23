524d6b0b4652a600ea9fa2480d6fd30db83563d2
Ben Hong <bencodezen@gmail.com>
2019-03-23 18:44:17 -0400

    :sparkles: Add pulse loader component

---
+++ src/components/PulseLoader.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <div class="is-spinning">
+    <AppIcon icon="spinner" size="lg" />
+  </div>
+</template>
+
+<style>
+@keyframes spinning {
+  from {
+    transform: rotate(0);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.is-spinning {
+  animation: spinning 2s infinite linear;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import AppButton from '@/components/AppButton'
+import PulseLoader from '@/components/PulseLoader'
 
 library.add(fas)
 
 Vue.component('AppIcon', FontAwesomeIcon)
 Vue.component('AppButton', AppButton)
+Vue.component('PulseLoader', PulseLoader)
 
 Vue.config.productionTip = false
 
