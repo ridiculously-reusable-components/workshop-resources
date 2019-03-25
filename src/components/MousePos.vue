a9be746c3d209eef2b49e6b9e6c68e41e7efc005
shentao <damian.dulisz@gmail.com>
2019-03-25 01:06:11 -0400

    Add MousePos example

---
+++ src/components/MousePos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div @mousemove="handleMouseMove">
+    <slot v-bind="{ x, y }"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      x: 0,
+      y: 0
+    }
+  },
+  methods: {
+    handleMouseMove (e) {
+      this.x = e.x
+      this.y = e.y
+    }
+  }
+}
+</script>
